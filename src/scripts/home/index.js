6d7b9fdd9103ed5954e15623198c71439a5229af
Emmanouela Siougkari <esiougkari@citycollege.sheffield.eu>
2023-06-09 20:16:45 +0300

    feat: add styling

---
+++ src/scripts/home/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var clientRows = document.getElementsByClassName('client-row');
+
+    for (var i = 0; i < clientRows.length; i++) {
+      clientRows[i].addEventListener('click', function() {
+        var clientData = this.nextElementSibling;
+        clientData.style.display = clientData.style.display === 'none' ? 'table-row' : 'none';
+      });
+    }
+  });
