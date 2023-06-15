87a1bb8eb1159eff9ebc755c7e0dba1002d65e3e
Lost <pandagaminggr@gmail.com>
2023-06-15 16:25:34 +0300

    feat: add entry page - disregard

---
+++ src/scripts/add/index.js
@@ -0,0 +1,7 @@
+document.getElementById("home").addEventListener("click", () => {
+    location.href = "/src/routes/home/index.html";
+});
+
+document.getElementById("add").addEventListener("click", () => {
+    location.href = "/src/routes/add/index.html";
+});
--- src/scripts/home/index.js
+++ src/scripts/home/index.js
@@ -1,32 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var clientRows = document.getElementsByClassName('client-row');
+document.addEventListener("DOMContentLoaded", function () {
+    var clientRows = document.getElementsByClassName("client-row");
 
     for (var i = 0; i < clientRows.length; i++) {
-      clientRows[i].addEventListener('click', function() {
-        var clientData = this.nextElementSibling;
-        clientData.style.display = clientData.style.display === 'none' ? 'table-row' : 'none';
-      });
+        clientRows[i].addEventListener("click", function () {
+            var clientData = this.nextElementSibling;
+            clientData.style.display = clientData.style.display === "none" ? "table-row" : "none";
+        });
     }
-  });
-
-  document.querySelectorAll('.edit-btn').forEach(function(button) {
-    button.addEventListener('click', function() {
-      alert('Edit button clicked');
-      // Perform edit operation here
-    });
-  });
-  
-  document.querySelectorAll('.delete-btn').forEach(function(button) {
-    button.addEventListener('click', function() {
-      alert('Delete button clicked');
-      // Perform delete operation here
-    });
-  });
-
-  
-  
-  
-  
-  
-  
-  
+});
